e1f1ae26d9aa14baf3273569e016b610698b4cf5
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-15 20:23:17 +0400

    Add wallet and recharge routes

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,32 +1,11 @@
 import 'package:flutter/material.dart';
 
 class NavigationService {
-  static final GlobalKey<NavigatorState> navigatorKey =
-      GlobalKey<NavigatorState>();
-
-  static Future<dynamic> navigateTo(String routeName, {dynamic arguments}) {
-    return navigatorKey.currentState!
-        .pushNamed(routeName, arguments: arguments);
-  }
-
-  static Future<dynamic> navigateToReplacement(String routeName,
-      {dynamic arguments}) {
-    return navigatorKey.currentState!
-        .pushReplacementNamed(routeName, arguments: arguments);
-  }
-
-  static Future<dynamic> navigateToAndRemoveUntil(String routeName,
-      {dynamic arguments}) {
-    return navigatorKey.currentState!.pushNamedAndRemoveUntil(
-      routeName,
-      (Route<dynamic> route) => false,
-      arguments: arguments,
-    );
-  }
-
-  static void goBack() {
-    return navigatorKey.currentState!.pop();
-  }
+  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  static Future<dynamic> navigateTo(String routeName, {dynamic arguments}) => navigatorKey.currentState!.pushNamed(routeName, arguments: arguments);
+  static Future<dynamic> navigateToReplacement(String routeName, {dynamic arguments}) => navigatorKey.currentState!.pushReplacementNamed(routeName, arguments: arguments);
+  static Future<dynamic> navigateToAndRemoveUntil(String routeName, {dynamic arguments}) => navigatorKey.currentState!.pushNamedAndRemoveUntil(routeName, (Route<dynamic> route) => false, arguments: arguments);
+  static void goBack() => navigatorKey.currentState!.pop();
 }
 
 class AppRoutes {
@@ -48,6 +27,8 @@ class AppRoutes {
   static const String register = '/register';
   static const String profile = '/profile';
   static const String settings = '/settings';
+  static const String wallet = '/wallet';
+  static const String recharge = '/recharge';
   static const String roomList = '/room-list';
   static const String createRoom = '/create-room';
   static const String voiceChatRoom = '/voice-chat-room';
