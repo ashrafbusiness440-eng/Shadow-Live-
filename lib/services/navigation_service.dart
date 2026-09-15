8d9331db371a5682ba8e26667352ab212c1b1ace
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-15 17:03:44 +0400

    Add recharge navigation routes

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,32 +1,12 @@
 import 'package:flutter/material.dart';
 
 class NavigationService {
-  static final GlobalKey<NavigatorState> navigatorKey =
-      GlobalKey<NavigatorState>();
+  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-  static Future<dynamic> navigateTo(String routeName, {dynamic arguments}) {
-    return navigatorKey.currentState!
-        .pushNamed(routeName, arguments: arguments);
-  }
-
-  static Future<dynamic> navigateToReplacement(String routeName,
-      {dynamic arguments}) {
-    return navigatorKey.currentState!
-        .pushReplacementNamed(routeName, arguments: arguments);
-  }
-
-  static Future<dynamic> navigateToAndRemoveUntil(String routeName,
-      {dynamic arguments}) {
-    return navigatorKey.currentState!.pushNamedAndRemoveUntil(
-      routeName,
-      (Route<dynamic> route) => false,
-      arguments: arguments,
-    );
-  }
-
-  static void goBack() {
-    return navigatorKey.currentState!.pop();
-  }
+  static Future<dynamic> navigateTo(String routeName, {dynamic arguments}) => navigatorKey.currentState!.pushNamed(routeName, arguments: arguments);
+  static Future<dynamic> navigateToReplacement(String routeName, {dynamic arguments}) => navigatorKey.currentState!.pushReplacementNamed(routeName, arguments: arguments);
+  static Future<dynamic> navigateToAndRemoveUntil(String routeName, {dynamic arguments}) => navigatorKey.currentState!.pushNamedAndRemoveUntil(routeName, (Route<dynamic> route) => false, arguments: arguments);
+  static void goBack() => navigatorKey.currentState!.pop();
 }
 
 class AppRoutes {
@@ -51,4 +31,6 @@ class AppRoutes {
   static const String roomList = '/room-list';
   static const String createRoom = '/create-room';
   static const String voiceChatRoom = '/voice-chat-room';
+  static const String recharge = '/recharge';
+  static const String rechargeCheckout = '/recharge-checkout';
 }
