16738c7df6f232d02e192cf752aca8ed447cd6c0
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-19 22:58:38 +0400

    Fix Shadow Control analyzer issue: lib/admin/control_config.dart

--- lib/admin/control_config.dart
+++ lib/admin/control_config.dart
@@ -4,7 +4,7 @@ class ControlConfig {
  static const minimumWithdrawalDiamonds=100.0;
  static const minimumAge=18;
  static const targetGameRtp=0.85;
- static const rechargePackages=<double,int>{
+ static final Map<double,int> rechargePackages={
   0.99:11000,1.99:23000,4.99:60000,9.99:125000,24.99:330000,49.99:700000,99.99:1500000,
  };
 }
