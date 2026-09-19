0678e1eb770d08b543842042afd46a8e8103acec
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-19 22:59:03 +0400

    Fix Shadow Control analyzer issue: lib/admin/control_wallet_security.dart

--- lib/admin/control_wallet_security.dart
+++ lib/admin/control_wallet_security.dart
@@ -1,5 +1,5 @@
 abstract final class WalletSecurityPolicy {
  static const sensitiveActions={'withdraw','convertDiamonds','giftDiamonds'};
  static bool requiresWalletPassword(String action)=>sensitiveActions.contains(action);
- static bool storePlaintextPassword=>false;
+ static bool get storePlaintextPassword=>false;
 }
