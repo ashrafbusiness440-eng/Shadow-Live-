52e381860124661f3c94d3848a8f3b31d7f5c418
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-19 22:58:56 +0400

    Fix Shadow Control analyzer issue: lib/admin/control_recharge.dart

--- lib/admin/control_recharge.dart
+++ lib/admin/control_recharge.dart
@@ -2,6 +2,6 @@ import 'control_config.dart';
 abstract final class RechargePolicy {
  static int? coinsForPackage(num usd)=>ControlConfig.rechargePackages[usd.toDouble()];
  static void validatePackage(num usd){if(coinsForPackage(usd)==null)throw ArgumentError('unknown recharge package');}
- static bool clientMayCreditBalance=>false;
- static bool requiresServerPurchaseVerification=>true;
+ static bool get clientMayCreditBalance=>false;
+ static bool get requiresServerPurchaseVerification=>true;
 }
