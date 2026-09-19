1215cedfb0986c81cc656d83c5bff59f44a7aa86
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-19 22:35:38 +0400

    Add privileged operation preflight: lib/admin/control_operation_codes.dart

---
+++ lib/admin/control_operation_codes.dart
@@ -0,0 +1,12 @@
+abstract final class ControlOperationCodes {
+ static const ok='ok';
+ static const denied='denied';
+ static const ownerProtected='owner_protected';
+ static const reauthRequired='reauth_required';
+ static const emergencyLocked='emergency_locked';
+ static const duplicate='duplicate';
+ static const invalidAmount='invalid_amount';
+ static const insufficientBalance='insufficient_balance';
+ static const invalidTransition='invalid_transition';
+ static const backendRequired='trusted_backend_required';
+}
