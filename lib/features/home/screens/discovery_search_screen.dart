c0d5ac8ab227e06ec21d343d9741b8e4698a6535
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-21 11:04:13 +0400

    Align room search with Phase 4 discovery policy

--- lib/features/home/screens/discovery_search_screen.dart
+++ lib/features/home/screens/discovery_search_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../../../services/navigation_service.dart';
 import '../../profile/screens/public_profile_screen.dart';
+import '../services/discovery_service.dart';
 
 class DiscoverySearchScreen extends StatefulWidget {
   const DiscoverySearchScreen({super.key});
@@ -24,7 +25,7 @@ class _DiscoverySearchScreenState extends State<DiscoverySearchScreen>{
       final id=await FirebaseFirestore.instance.collection('public_ids').doc(q).get();
       if(id.exists){final targetUid=id.data()?['uid']?.toString();if(targetUid!=null&&targetUid!=_uid){final u=await FirebaseFirestore.instance.collection('public_profiles').doc(targetUid).get();if(u.exists)people.add({...?u.data(),'uid':targetUid});}}
       try{final users=await FirebaseFirestore.instance.collection('public_profiles').orderBy('displayName').startAt([q]).endAt(['$q\uf8ff']).limit(12).get();for(final d in users.docs){if(d.id!=_uid&&!people.any((x)=>x['uid']==d.id))people.add({...d.data(),'uid':d.id});}}catch(_){}
-      try{final rs=await FirebaseFirestore.instance.collection('rooms').orderBy('name').startAt([q]).endAt(['$q\uf8ff']).limit(12).get();for(final d in rs.docs){rooms.add({...d.data(),'roomId':d.id});}}catch(_){}
+      try{final rs=await FirebaseFirestore.instance.collection('rooms').orderBy('name').startAt([q]).endAt(['$q\uf8ff']).limit(12).get();for(final d in rs.docs){final room=DiscoveryRoom(id:d.id,data:d.data());if(room.isActive&&!room.isHidden)rooms.add(room.toNavigationArguments());}}catch(_){}
       if(mounted)setState((){_people=people;_rooms=rooms;});
     }catch(_){if(mounted)setState(()=>_error='تعذر البحث حالياً. حاول مرة أخرى.');}
     finally{if(mounted)setState(()=>_loading=false);}
