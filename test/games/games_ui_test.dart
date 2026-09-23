df01f08b0bfae8fb6b4950f5cd82054dc1623290
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-23 17:33:50 +0400

    Fix Greedy Cat widget test scrolling

--- test/games/games_ui_test.dart
+++ test/games/games_ui_test.dart
@@ -29,7 +29,7 @@ void main() {
     await tester.pump();
 
     final joinButton = find.textContaining('شارك بـ 200');
-    await tester.ensureVisible(joinButton);
+    await tester.scrollUntilVisible(joinButton, 300);
     await tester.pumpAndSettle();
     await tester.tap(joinButton);
     await tester.pump();
