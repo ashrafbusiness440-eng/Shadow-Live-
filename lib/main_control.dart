0f6429be7de9e218df86c192d8faacd271fdf26b
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-23 09:28:34 +0400

    test(control): capture economy page in final browser E2E

--- lib/main_control.dart
+++ lib/main_control.dart
@@ -31,7 +31,7 @@ class ShadowControlApp extends StatelessWidget {
     home: const Directionality(
       textDirection: TextDirection.rtl,
       child: bool.fromEnvironment('CONTROL_E2E_TEST')
-          ? ControlShell(initialNavIndex: 2)
+          ? ControlShell(initialNavIndex: 3)
           : AdminGate(),
     ),
   );
