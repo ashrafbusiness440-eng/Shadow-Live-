a9842a120043a1c688b16b62f25ef61803727cc7
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-25 09:29:59 +0400

    chore(auth): remove unused setup import

--- lib/features/auth/screens/email_login_screen.dart
+++ lib/features/auth/screens/email_login_screen.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../bloc/auth_bloc.dart';
-import '../setup_route.dart';
 import '../../../services/navigation_service.dart';
 
 class EmailLoginScreen extends StatefulWidget {
