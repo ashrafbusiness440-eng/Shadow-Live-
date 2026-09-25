24f9e23aa35b69cd304b88c5c824f2377f2de592
ashrafbusiness440-eng <ashraf.business.440@gmail.com>
2026-09-25 17:02:49 +0400

    cloudflare: fix daily winnings settlement reference

--- cloudflare-worker/src/legacy-games/game-runtime.js
+++ cloudflare-worker/src/legacy-games/game-runtime.js
@@ -377,9 +377,6 @@ export async function placeGameBet(
     const creditLedgerRef=db.collection("financial_ledger").doc("game_credit__"+operationId);
     const historyRef=db.collection("game_user_history")
       .doc(uid).collection("items").doc(operationId);
-    const dailyStatsRef=db.collection("game_user_stats")
-      .doc(uid).collection("daily")
-      .doc(clean(operation.gameId)+"__"+clean(operation.dayKey));
 
     tx.update(userRef,{
       coins:finalBalance,
@@ -543,6 +540,9 @@ async function settleOperationRef(db,operationRef,nowMs,{workerTag=""}={}){
     const creditLedgerRef=db.collection("financial_ledger").doc("game_credit__"+operationId);
     const historyRef=db.collection("game_user_history")
       .doc(uid).collection("items").doc(operationId);
+    const dailyStatsRef=db.collection("game_user_stats")
+      .doc(uid).collection("daily")
+      .doc(clean(operation.gameId)+"__"+clean(operation.dayKey));
 
     tx.update(userRef,{
       coins:after,
